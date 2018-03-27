82222cd590c3c655aeb88c957d84b7323d356874
moogun <moogunjung@gmail.com>
2018-03-27 11:40:48 +0900

    New Q became class component

--- src/components/questions/NewQ.js
+++ src/components/questions/NewQ.js
@@ -3,35 +3,43 @@ import React, { Component } from 'react'
 import { Segment, Container, Button, Header, Icon, Menu, Divider, Card, Image, Grid, Form, Item, Label, Rating, TextArea, Input, Breadcrumb } from 'semantic-ui-react'
 // import ImageUpload from './ImageUpload'
 
-const NewQ = ({mobile}) => (
-  <div>
-    <Segment basic>
-      <Container text>
-        <Breadcrumb>
-          <Breadcrumb.Section>Teacher name</Breadcrumb.Section>
-          <Breadcrumb.Divider icon='right angle' />
-          <Breadcrumb.Section>Course name</Breadcrumb.Section>
-          <Breadcrumb.Divider icon='right angle' />
-        </Breadcrumb>
-          <br/><br/>
-        <Form>
-          <Form.Field>
-            <input placeholder='Enter title' />
-          </Form.Field>
-          <Form.Field>
-            {/* <ImageUpload /> */}
-          </Form.Field>
-          <Form.Field>
-            <TextArea placeholder='Tell us more' />
-          </Form.Field>
-          <Button type='submit'>Cancel</Button>
-          <Button type='submit'>Submit</Button>
-          </Form>
+class NewQ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-      </Container>
-    </Segment>
-  </div>
-)
+    };
+  }
+  render() {
+    return (
+      <Segment basic>
+        <Container text>
+          <Breadcrumb>
+            <Breadcrumb.Section>Teacher name</Breadcrumb.Section>
+            <Breadcrumb.Divider icon='right angle' />
+            <Breadcrumb.Section>Course name</Breadcrumb.Section>
+            <Breadcrumb.Divider icon='right angle' />
+          </Breadcrumb>
+            <br/><br/>
+          <Form>
+            <Form.Field>
+              <input placeholder='Enter title' />
+            </Form.Field>
+            <Form.Field>
+              {/* <ImageUpload /> */}
+            </Form.Field>
+            <Form.Field>
+              <TextArea placeholder='Tell us more' />
+            </Form.Field>
+            <Button>Cancel</Button>
+            <Button type='submit'>Submit</Button>
+            </Form>
+
+        </Container>
+      </Segment>
+    );
+  }
+}
 
 NewQ.propTypes = {
   mobile: PropTypes.bool,
