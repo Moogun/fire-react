d37d0a4535591feccb08377ed741351f7e12a53c
moogun <moogunjung@gmail.com>
2018-04-02 12:23:39 +0900

    fixed nav to have default style + border radius 0px acrosss desktop and mobile, auth, non auth

--- src/components/navbar/NavAuth.js
+++ src/components/navbar/NavAuth.js
@@ -6,6 +6,8 @@ import {auth, db} from '../../firebase'
 
 import { Responsive, Visibility, Segment, Container, Menu, Icon, Grid, Input, Button, Dropdown, Header, Modal, Feed, Sidebar, Form, Divider, Checkbox } from 'semantic-ui-react'
 
+const menuStyle ={borderRadius: '0px'}
+
 class NavAuth extends Component {
 
   state = {}
@@ -35,46 +37,50 @@ class NavAuth extends Component {
 
         <Responsive {...Responsive.onlyComputer}>
           <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
-            <Menu style={{borderRadius: '0rem', padding: '0.5rem'}} secondary>
-              <Container color='blue'>
+
+            <Menu style={menuStyle}>
+              <Container>
 
                   <Menu.Item as={Link} to={routes.HOME}
                     name='Weqna' active={activeItem === 'home'} onClick={this.handleItemClick} />
 
                   <Category />
 
-                  <Dropdown item text='Teacher' >
-                    <Dropdown.Menu>
-                      <Dropdown.Item as={Link} to={routes.CREATE}
-                        name='create' active={activeItem === 'create'} onClick={this.handleItemClick}>
-                        Create New Course
-                      </Dropdown.Item>
-                      <Dropdown.Divider />
-                      <Dropdown.Item as={Link} to={routes.DASHBOARD}
-                        name='dashboard'
-                        active={activeItem === 'dashboard'}
-                        onClick={this.handleItemClick}
-                        >
-                          Manage Courses
+                  <Menu.Item>
+                      <Input placeholder='Search...' action={{ icon: 'search' }}/>
+                  </Menu.Item>
 
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
+                  <Menu.Menu  position='right'>
+                    <Dropdown item text='Teacher' >
+                      <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to={routes.CREATE}
+                          name='create' active={activeItem === 'create'} onClick={this.handleItemClick}>
+                          Create New Course
+                        </Dropdown.Item>
+                        <Dropdown.Item as={Link} to={routes.DASHBOARD}
+                          name='dashboard'
+                          active={activeItem === 'dashboard'}
+                          onClick={this.handleItemClick}
+                          >
+                            Manage Courses
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
 
-                  <Menu.Item as={Link} to={routes.MY_COURSES}
-                    name='my courses' active={activeItem === 'mycourses'} onClick={this.handleItemClick} />
+                    <Menu.Item as={Link} to={routes.MY_COURSES}
+                      name='my courses' active={activeItem === 'mycourses'} onClick={this.handleItemClick} />
 
-                  <Dropdown item text='Account' >
-                    <Dropdown.Menu>
-                      <Dropdown.Item as={Link} to={routes.ACCOUNT}>
-                        {authUser.email}
-                      </Dropdown.Item>
-                      <Dropdown.Item onClick={auth.doSignOut}>
-                           SignOut
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-
+                    <Dropdown item text='Account' >
+                      <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to={routes.ACCOUNT}>
+                          {authUser.email}
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={auth.doSignOut}>
+                             SignOut
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
              </Container>
            </Menu>
            </Visibility>
@@ -82,43 +88,30 @@ class NavAuth extends Component {
 
 
          <Responsive {...Responsive.onlyMobile}>
-           <Sidebar.Pushable>
-              <Sidebar as={Menu} animation='overlay' inverted vertical visible={sidebarOpened}
-                >
-                <Menu.Item as='a'>{authUser.email}</Menu.Item>
-                <Menu.Item as='a'>Categories</Menu.Item>
-                <Menu.Item as='a'>My Courses</Menu.Item>
-                <Menu.Item as='a'>My Messages</Menu.Item>
-                <Menu.Item as='a'>My Wishlist</Menu.Item>
-                <Menu.Item as='a'>Settings</Menu.Item>
-                <Menu.Item as='a'>Help</Menu.Item>
-                <Menu.Item onClick={auth.doSignOut} >Logout</Menu.Item>
-              </Sidebar>
-
-              <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
-                <Grid celled centered style={{padding: '4em'}}>
-                  <Grid.Column width={3}  textAlign='center'>
-                      <Menu.Item fitted name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
-                      <Icon name='question'/> We Qna
-                      </Menu.Item>
-                    </Grid.Column>
-                    <Grid.Column width={9} >
-                     <Input transparent fluid icon='search' iconPosition='left' placeholder='Search...' />
-                  </Grid.Column>
-                  <Grid.Column width={2} textAlign='center'>
-                    <Menu.Item name='signup' active={activeItem === 'signup'} onClick={this.handleItemClick} >
+           <Menu style={menuStyle} icon size='tiny'>
+             <Container>
+               <Menu.Item  as={Link} to={routes.LANDING}
+                 active={activeItem === 'weqna'} onClick={this.handleItemClick}> <Icon name='gamepad' /> </Menu.Item>
+               <Menu.Item  as={Link} to={routes.LANDING}
+                 active={activeItem === 'weqna'} onClick={this.handleItemClick}> <Icon name='browser' /> </Menu.Item>
+               <Menu.Item>
+                   <Input placeholder='Search...' action={{ icon: 'search' }}/>
+               </Menu.Item>
+               <Menu.Menu position='right'>
+                 <Menu.Item>
                      <Icon name='book' />
-                    </Menu.Item>
-                  </Grid.Column>
-                <Grid.Column width={2} textAlign='center'>
-                    <Menu.Item name='help' active={activeItem === 'help'} onClick={this.handleItemClick}>
-                     <Icon name='user' />
-                    </Menu.Item>
-                 </Grid.Column>
-              </Grid>
-              </Sidebar.Pusher>
-            </Sidebar.Pushable>
+                 </Menu.Item>
+                 <Menu.Item as={Link} to={routes.SIGN_IN}
+                   active={activeItem === 'signin'}
+                   onClick={this.handleItemClick}
+                    >
+                      <Icon name='user circle' />
+                 </Menu.Item>
 
+               </Menu.Menu>
+            </Container>
+
+          </Menu>
          </Responsive>
 
      </div>
--- src/components/navbar/NavNonAuth.js
+++ src/components/navbar/NavNonAuth.js
@@ -4,117 +4,80 @@ import * as routes from '../../constants/routes';
 import Category from './Category'
 import AuthModal from '../account/AuthModal'
 
-import { Responsive, Visibility, Segment, Container, Menu, Icon, Grid, Input, Button, Dropdown, Header, Modal, Feed, Sidebar, Form, Divider, Checkbox } from 'semantic-ui-react'
+import { Responsive, Visibility, Segment, Container, Menu, Icon, Grid, Input, Button, Dropdown, Header, Modal, Feed, Sidebar, Form, Divider, Checkbox, } from 'semantic-ui-react'
 
+const menuStyle ={borderRadius: '0px'}
 class NavNonAuth extends Component {
 
   state = {
     activeItem: '',
-    authReq: null,
   }
   //
   // hideFixedMenu = () => this.setState({ fixed: false })
   // showFixedMenu = () => this.setState({ fixed: true })
   //
-  // handlePusherClick = () => {
-  //   const { sidebarOpened } = this.state
-  //   if (sidebarOpened) this.setState({ sidebarOpened: false })
-  // }
-  //
-  // handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
-  test = () => this.setState ({ authReq: 'login'})
-  handleToLogIn = () => this.setState ({ authReq: 'login' })
-  handleToSignUp = () => this.setState ({ authReq: 'signup' })
+
   handleSearchField = () => this.setState ({ searchFieldActive: !this.state.searchFieldActive})
 
   render() {
 
     const {activeItem, searchFieldActive, sidebarOpened} = this.state
-    // , authReq
-    // let authModalHeader = authReq === 'signin' ? "Login" : "Sign Up"
-    // console.log('auth req', this.state.authReq);
 
     let searchField = searchFieldActive ? <Grid.Column><Input className='icon' icon='search' placeholder='Search...' fluid transparent iconPosition='left'/></Grid.Column> : null
 
     return (
       <div>
         <Responsive {...Responsive.onlyComputer}>
-            <Menu >
+            <Menu style={menuStyle}>
               <Container>
                 <Menu.Item  as={Link} to={routes.LANDING}
-                  name='landing' active={activeItem === 'landing'} onClick={this.handleItemClick} />
+                  active={activeItem === 'weqna'} onClick={this.handleItemClick}> We qna </Menu.Item>
                   <Category />
+                <Menu.Item>
+                    <Input placeholder='Search...' action={{ icon: 'search' }}/>
+                </Menu.Item>
 
-                  <Menu.Menu position='right'>
-                    <Menu.Item as={Link} to={routes.SIGN_IN}
-                      name='Sign In' active={activeItem === 'signin'}
-                      onClick={this.handleItemClick}
-                       >
-                    </Menu.Item>
+                <Menu.Menu position='right'>
+                  <Menu.Item as={Link} to={routes.SIGN_IN}
+                    name='Sign In' active={activeItem === 'signin'}
+                    onClick={this.handleItemClick}
+                     >
+                  </Menu.Item>
 
-                    <Menu.Item as={Link} to={routes.SIGN_UP}
-                      name='Sign Up' active={activeItem === 'signup'}
-                      onClick={this.handleItemClick}
-                       >
-                    </Menu.Item>
+                  <Menu.Item as={Link} to={routes.SIGN_UP}
+                    name='Sign Up' active={activeItem === 'signup'}
+                    onClick={this.handleItemClick}
+                     >
+                  </Menu.Item>
 
-                  </Menu.Menu>
+                </Menu.Menu>
              </Container>
 
            </Menu>
         </Responsive>
 
         <Responsive {...Responsive.onlyMobile}>
-          <Sidebar.Pushable>
+          <Menu style={menuStyle} icon size='tiny'>
+            <Container>
+              <Menu.Item  as={Link} to={routes.LANDING}
+                active={activeItem === 'weqna'} onClick={this.handleItemClick}> <Icon name='gamepad' /> </Menu.Item>
+              <Menu.Item  as={Link} to={routes.LANDING}
+                active={activeItem === 'weqna'} onClick={this.handleItemClick}> <Icon name='browser' /> </Menu.Item>
+              <Menu.Item>
+                  <Input placeholder='Search...' action={{ icon: 'search' }}/>
+              </Menu.Item>
+              <Menu.Menu position='right'>
+                <Menu.Item as={Link} to={routes.SIGN_IN}
+                  active={activeItem === 'signin'}
+                  onClick={this.handleItemClick}
+                   >
+                     <Icon name='signup' />
+                </Menu.Item>
 
-             <Sidebar as={Menu} animation='overlay' inverted vertical visible={sidebarOpened}>
-               <Menu.Item as='a'>Categories</Menu.Item>
-               <Menu.Item as='a'>Register Courses</Menu.Item>
-               <Menu.Item as='a'>Help</Menu.Item>
-             </Sidebar>
+              </Menu.Menu>
+           </Container>
 
-             <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh', margin: '0em' }}>
-
-               <Grid celled style={{margin: '0em 0em'}}>
-                <Grid.Row>
-                 <Grid.Column width={2}  textAlign='center'>
-                   <Menu.Item onClick={this.handleToggle}>
-                     <Icon name='sidebar' />
-                   </Menu.Item>
-                 </Grid.Column>
-
-                 <Grid.Column width={2}  textAlign='center'>
-                   <Menu.Item onClick={this.handleSearchField}>
-                     <Icon name='search'/>
-                   </Menu.Item>
-                 </Grid.Column>
-
-                 <Grid.Column width={8}  textAlign='center'>
-                     <Menu.Item fitted name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
-                     <Icon name='question'/> We Qna
-                     </Menu.Item>
-                  </Grid.Column>
-
-                 <Grid.Column width={2} textAlign='center'>
-                   <Menu.Item name='signup' active={activeItem === 'signup'} onClick={this.handleItemClick} >
-                    <Icon name='book' />
-                   </Menu.Item>
-                 </Grid.Column>
-
-                 <Grid.Column width={2} textAlign='center'>
-                   <Menu.Item name='help' active={activeItem === 'help'} onClick={this.handleItemClick}>
-                    <Icon name='user' />
-                   </Menu.Item>
-                </Grid.Column>
-
-                </Grid.Row>
-                <Grid.Row>
-                  {searchField}
-                </Grid.Row>
-              </Grid>
-
-             </Sidebar.Pusher>
-           </Sidebar.Pushable>
+         </Menu>
         </Responsive>
 
       </div>
