d64249a3d022b9a8f97d7cd100bf6382a09e98f9
moogun <moogunjung@gmail.com>
2018-04-04 11:22:44 +0900

    added multiple images upload feature

--- src/components/courseManage/CEditGallery.js
+++ src/components/courseManage/CEditGallery.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Form, Input, Button, Segment, Icon, Header, Divider, Image, Grid, Dimmer} from 'semantic-ui-react'
+import { Form, Input, Button, Segment, Icon, Header, Divider, Image, Grid, Dimmer, Progress, Confirm} from 'semantic-ui-react'
 import {db} from '../../firebase';
 import {storage} from '../../firebase/firebase';
 
@@ -20,7 +20,7 @@ class CEditGallery extends Component {
   constructor(props) {
   super(props);
   this.state = {
-
+      images: {},
     }
     this.textInput = null;
     this.setTextInputRef = element => {
@@ -35,6 +35,9 @@ class CEditGallery extends Component {
   handleHide = () => this.setState({ active: false })
 
   handleChange = (e) => {
+    const {images} = this.state
+    let newKey = db.newKey();
+
     console.log(e.target.files[0]);
     let reader = new FileReader()
     let file =  e.target.files[0]
@@ -49,117 +52,132 @@ class CEditGallery extends Component {
       //     let localUrl = reader.readAsDataURL(file[f])
       //   })
       //  }
-
-
+      images[newKey] = { file: file, imagePreviewUrl: reader.result}
+      this.setState ({images})
       // this.setState({
-      //   file: file,
-      //   imagePreviewUrl: reader.result
-      // });
-
-
-      this.setState({
-          file: file,
-          imagePreviewUrl: [reader.result],
-      })
+      //     file: file,
+      //     imagePreviewUrl: [reader.result],
+      // })
     }
+
     if(e.target.files[0]){
       reader.readAsDataURL(file)
-   }
+    }
+
   }
 
   handleSubmit = () => {
-    let newKey = db.newKey();
-    const {file} = this.state
-    console.log('newkey', newKey, 'file', file);
+    // let newKey = db.newKey();
+    const {images} = this.state
+    // console.log('newkey', newKey, 'file', file);
     const {teacherId, courseId } = this.props
-    var uploadTask = storage.ref().child('images').child(newKey).put(file);
 
-    uploadTask.on('state_changed', function(snapshot){
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case 'paused': // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case 'running': // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error) {
-      // Handle unsuccessful uploads
-        switch (error.code) {
-           case 'storage/unauthorized':
-             // User doesn't have permission to access the object
-             break;
-           case 'storage/canceled':
-             // User canceled the upload
-             break;
-           case 'storage/unknown':
-             // Unknown error occurred, inspect error.serverResponse
-             break;
-         }
-    }, function() {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      var downloadURL = uploadTask.snapshot.downloadURL;
-      console.log('down', downloadURL);
-      db.doUpdateCourseImages(teacherId, courseId, newKey, downloadURL)
-        .then(res => console.log('res', res))
-        .catch(error => {
-          this.setState(byPropKey('error', error));
-        });
-    });
+    Object.keys(images).map(i => {
+      console.log('images[i]', 'i', i, 'file', images[i].file)
+
+      var uploadTask= storage.ref().child('images').child(i).put(images[i].file)
+      uploadTask.on('state_changed', function(snapshot){
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused': // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case 'running': // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+          switch (error.code) {
+             case 'storage/unauthorized':
+               // User doesn't have permission to access the object
+               break;
+             case 'storage/canceled':
+               // User canceled the upload
+               break;
+             case 'storage/unknown':
+               // Unknown error occurred, inspect error.serverResponse
+               break;
+           }
+      }, function() {
+        var downloadURL = uploadTask.snapshot.downloadURL;
+        console.log('down', downloadURL);
+        db.doUpdateCourseImages(teacherId, courseId, i, downloadURL)
+          .then(res => console.log('res', res))
+          .catch(error => {
+            this.setState(byPropKey('error', error));
+          });
+      });
+    })
   }
 
-  handleClick2 = () => {
-
+  show = (id) => {
+    console.log(id);
+    this.setState ({ open: true, selectedImage: id})
+  }
+  handleConfirm = () => {
+    const {images, selectedImage} = this.state
+    let newImages = delete images[selectedImage]
+    this.setState({ open: false, newImages })
+  }
+  handleCancel = () => {
+    this.setState({ open: false })
   }
 
   render() {
-    console.log('image preview url', this.state.imagePreviewUrl);
-    const { active } = this.state
+
+    const {images } = this.state
+
+    let img = Object.keys(images).map(i => images[i].imagePreviewUrl)
+    console.log('img', img);
 
     const content = (
-    <div>
-      <Button color='red'>Remove?</Button>
-    </div>
-  )
-
-    let {imagePreviewUrl} = this.state;
-       let $imagePreview = null;
-       if (imagePreviewUrl) {
-         $imagePreview = (
-         <Dimmer.Dimmable
-          as={Image}
-          dimmed={active}
-          dimmer={{ active, content }}
-          onMouseEnter={this.handleShow}
-          onMouseLeave={this.handleHide}
-          size='medium'
-          src={imagePreviewUrl}
-        />
-      );
-       } else {
-         $imagePreview = (<div>Please select an Image for Preview</div>);
-       }
+      <div>
+        <Button color='red'>Remove?</Button>
+      </div>
+    )
 
     return (
       <Segment style={CEditMetaBorder}>
+        <Header as='h2'>Gallery</Header>
+        <Divider />
+        <Segment basic style={minHeight} textAlign='center'>
+          <Grid columns={3}>
+            {Object.keys(images).map(i =>
+              <Grid.Column>
+                <Dimmer.Dimmable
+                    as={Image}
+                    onClick={() => this.show(i)}
+                    size='medium'
+                    src={images[i].imagePreviewUrl}
+                  />
+                </Grid.Column>
+            )}
+            <Grid.Column>
+              <Button icon='image' basic onClick={this.focusTextInput} size='massive' fluid style={{height: '100%'}}/>
 
-        {$imagePreview}
+              <Confirm
+                open={this.state.open}
+                content='Remove image?'
+                onCancel={this.handleCancel}
+                onConfirm={this.handleConfirm}
+              />
+
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        <Divider />
 
         <input placeholder='iage...'
-          type='file' input={{ accept: 'image/*', multiple: 'true'}}
+          type='file'
+          accept='image/*'
+
           ref={this.setTextInputRef}
           style={{display: "none"}}
-        onChange={(e) => this.handleChange(e)}
-       />
-        <Button onClick={this.handleSubmit}>Save</Button>
-
-        <Button onClick={this.focusTextInput}>open up</Button>
-
+          onChange={(e) => this.handleChange(e)} />
+        <Button primary onClick={this.handleSubmit}>Save</Button>
+        {/* <Progress percent={11} /> */}
       </Segment>
     );
   }
