1f5f087c5968835bbaf767a6c8dadc7beb9b5a60
moogun <moogunjung@gmail.com>
2018-05-09 13:55:20 +0900

    firebase functions,  admins, and tools updated. cloud functions are being tested

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,27 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
+
+exports.makeUppercase = functions.database.ref('/users/{uid}/username')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.uid, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
