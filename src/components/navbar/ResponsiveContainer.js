6a1a63e9e6d0e4d34b7e4f5bf9b8c6451d1c3d8d
moogun <moogunjung@gmail.com>
2018-05-10 16:54:58 +0900

    routes in App were moved to responsive containers, but routes in my course page reload together

--- src/components/navbar/ResponsiveContainer.js
+++ src/components/navbar/ResponsiveContainer.js
@@ -16,6 +16,36 @@ import SignInPage from '../account/SignIn';
 import PasswordForgetPage from '../account/PasswordForget';
 import AccountPage from '../account/Account';
 
+import TeacherIntro from '../teacherIntro/TeacherIntro';
+
+import Teacher from '../teacher/Teacher';
+import Courses from '../teacher/Courses';
+import Question from '../questionPage/QuestionPage';
+
+
+import CEdit from '../courseManage/CEdit';
+// import Create from '../courseManage/Create';
+import Create from '../courseManage/CreateC';
+import CoursePage from '../coursePage/CoursePage';
+import MyCoursePage from '../coursePageMy/MyCoursePage';
+
+
+import MyCourses from '../myCourses/MyCourses';
+
+
+import Dashboard from '../dashboard/Dashboard';
+
+
+import QuizEdit from '../quiz/QuizEdit'
+
+
+import Footer from '../footer/Footer';
+import About from '../footer/About';
+import Faq from '../footer/Faq';
+import Terms from '../footer/Terms';
+import Help from '../footer/Help';
+
+
 import AuthContainer from './AuthContainer'
 import NonAuthContainer from './NonAuthContainer'
 
@@ -51,9 +81,42 @@ class ResponsiveContainer extends Component {
               />
 
               <AppRoute
-                exact path={routes.ACCOUNT} layout={AltLayout} authUser={authUser}
+                exact path={routes.ACCOUNT} layout={MainLayout} authUser={authUser}
                 component={() => <AccountPage />}
               />
+
+
+              <AppRoute exact path={routes.LEARNING} layout={MainLayout} authUser={authUser}
+                component={() => <MyCourses />} />
+
+              <AppRoute exact path={routes.TEACHER_INTRO} layout={MainLayout} authUser={authUser} component={() => <TeacherIntro />} />
+
+              <AppRoute
+                exact path={routes.CREATE} layout={MainLayout} authUser={authUser}
+                component={() => <Create />} />
+
+              <AppRoute path={routes.COURSE_MANAGE} layout={MainLayout} authUser={authUser} component={() => <CEdit />} />
+
+              <AppRoute path={routes.DASHBOARD} layout={MainLayout} authUser={authUser} component={() => <Dashboard />} />
+
+              <AppRoute path={routes.QUIZ_MANAGE} layout={MainLayout} authUser={authUser} component={() => <QuizEdit />} />
+
+              <AppRoute
+                path={routes.TEACHER_PAGE} layout={MainLayout} authUser={authUser} component={() => <Teacher />} />
+
+              <AppRoute
+                exact path={routes.COURSE_PAGE} layout={MainLayout} authUser={authUser} component={() => <CoursePage />} />
+
+              <AppRoute
+                path={routes.MY_COURSE_PAGE} layout={MainLayout} authUser={authUser} component={() => <MyCoursePage />} />
+
+              <AppRoute exact path={routes.FOOTER_ABOUT} layout={MainLayout} authUser={authUser} component={() => <About />} />
+              <AppRoute exact path={routes.FOOTER_TERMS} layout={MainLayout} authUser={authUser} component={() => <Terms />} />
+              <AppRoute exact path={routes.FOOTER_FAQ} layout={MainLayout} authUser={authUser} component={() => <Faq />} />
+              <AppRoute exact path={routes.FOOTER_HELP} layout={MainLayout} authUser={authUser} component={() => <Help />} />
+
+              {/* <AppRoute layout={MainLayout} component={NoMatch} /> */}
+
         </div>
     );
   }
@@ -85,19 +148,33 @@ const MainLayout = (props) => {
       {props.authUser
         ? <AuthContainer authUser={props.authUser}> {props.children}   </AuthContainer>
         : <NonAuthContainer> {props.children} </NonAuthContainer> }
-
-      {props.children}
       <p>Footer</p>
     </div>
   )
 }
 
-  const AltLayout = (props) => (
+  const AltLayoutFor = (props) => (
     <div>
       <h1>Alt</h1>
       {props.authUser
         ? <p>auth container 2</p>
         : <p>non auth container 2 </p> }
-      {props.children}
     </div>
   )
+
+  const NoMatch = ({ location }) => (
+    <Grid verticalAlign='middle' textAlign='center' style={style.NO_MATCH_MIN_HEIGHT}>
+      <Grid.Row>
+      <Grid.Column>
+
+        <Header as='h1' style={{fontSize: '4rem'}}>
+          Sorry!
+          <Header.Subheader>
+            We can't find the page you're looking for.
+            Visit our support page for any questions. (404)
+          </Header.Subheader>
+        </Header>
+      </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
